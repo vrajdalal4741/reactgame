8b677566e4123a1721451746067de55257d326b0
vrajdalal4741 <vrajdalal.popeyes@gmail.com>
2018-04-17 15:09:50 -0400

    fourth update

--- src/App.js
+++ src/App.js
@@ -16,12 +16,70 @@ class App extends Component {
     navNote: "Click on a Running Back to begin!"
   };
 
+  Shuffle = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    };
+  };
 
+  gameWon = score => {
+    if (score === 11) {
+      const images = this.state.images;
+      for (let i = 0; i < images.length; i++) {
+        images[i].clicked = false
+      };
+        if(this.state.currentScore > this.state.maxScore){
+          this.setState({ maxScore: this.state.currentScore + 1 });
+
+        };
+
+        this.setState({ images });
+        this.setState({ navNote: "YOU JUST WON!!" });
+        this.setState({ currentScore: 0});
+
+    };
+  };
+
+  updateState = id => {
+
+    const Image = this.state.images.find(image => image.id === id);
+
+    if(Image.clicked === false) {
+
+      Image.clicked = true;
+      this.Shuffle(images);
+
+      this.setState({ images });
+      this.setState({ navNote: "Nice! Continue choosing a different RB!" });
+      this.setState({ currentScore: this.state.currentScore + 1 });
+      this.gameWon(this.state.currentScore);
+
+    } else {
+
+        const images = this.state.images
+
+        for (let i = 0; i < images.length; i++) {
+          images[i].clicked = false
+
+        };
+
+        if(this.state.currentScore > this.state.maxScore){
+          this.setState({ maxScore: this.state.currentScore });
+
+        }
+
+        this.setState({ images });
+        this.setState({ navNote: "OH NO! You clicked on that back already!" });
+        this.setState({ currentScore: 0});
+    };
+
+  };
 
   render() {
     return (
      <div>
-      <Nav 
+      <Nav
         navNote={this.state.navNote} 
         currentScore={this.state.currentScore} 
         maxScore={this.state.maxScore} 
